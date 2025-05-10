951a89864609242e09ba32f07defc980b1e33123
Khushi Jain <khushijain3579@gmail.com>
2025-05-10 23:04:10 +0530

    commit final

---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+ function toggleSidebar() {
+    const sidebar = document.getElementById("Sidebar");
+     sidebar.classList.toggle("show");
+    // sidebar.classList.toggle("d-none");
+    // sidebar.classList.toggle("d-flex");
+  }
+
