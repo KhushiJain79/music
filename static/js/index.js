cec72eb7c2b98195ce92279990231284c8b97814
Khushi Jain <khushijain3579@gmail.com>
2025-05-11 21:37:18 +0530

    changes

--- static/js/index.js
+++ static/js/index.js
@@ -5,10 +5,16 @@
     // sidebar.classList.toggle("d-flex");
   }
 
-
   const toggleBtn = document.getElementById('themeToggle');
   toggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('light-theme');
     toggleBtn.textContent = document.body.classList.contains('light-theme') ? '🌞' : '🌙';
+    const waves=document.getElementById('wave');
+      if (document.body.classList.contains('light-theme')) {
+    waves.src = 'https://cdn-icons-png.flaticon.com/128/4287/4287943.png';
+  } else {
+     waves.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO_d-O6ZaaUOFD4XziP70sQXRcl1pUXTNwfg&s';
+   
+  }
   });
 
