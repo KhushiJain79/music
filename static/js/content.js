8f134e979f8660879fd0799b5094391b5332f2a9
Khushi Jain <khushijain3579@gmail.com>
2025-05-11 20:14:59 +0530

    changes

--- static/js/content.js
+++ static/js/content.js
@@ -1,54 +1,54 @@
- window.onload = function () {
-        const video = document.getElementById('video');
-        const canvas = document.getElementById('canvas');
-        const resultText = document.getElementById('result');
-        const cameraBtn = document.getElementById('cameraBtn');
+//  window.onload = function () {
+//         const video = document.getElementById('video');
+//         const canvas = document.getElementById('canvas');
+//         const resultText = document.getElementById('result');
+//         const cameraBtn = document.getElementById('cameraBtn');
 
-        let cameraOn = false;
-        let stream = null;
+//         let cameraOn = false;
+//         let stream = null;
 
-        cameraBtn.onclick = async function () {
-            if (!cameraOn) {
-                try {
-                    stream = await navigator.mediaDevices.getUserMedia({ video: true });
-                    video.srcObject = stream;
-                    video.classList.remove('hidden');
-                    cameraBtn.textContent = "Capture Mood";
-                    cameraOn = true;
-                } catch (err) {
-                    alert("Camera not available: " + err);
-                }
-            } else {
-                // Capture the image
-                const context = canvas.getContext('2d');
-                context.drawImage(video, 0, 0, canvas.width, canvas.height);
-                const imageData = canvas.toDataURL('image/jpeg');
+//         cameraBtn.onclick = async function () {
+//             if (!cameraOn) {
+//                 try {
+//                     stream = await navigator.mediaDevices.getUserMedia({ video: true });
+//                     video.srcObject = stream;
+//                     video.classList.remove('hidden');
+//                     cameraBtn.textContent = "Capture Mood";
+//                     cameraOn = true;
+//                 } catch (err) {
+//                     alert("Camera not available: " + err);
+//                 }
+//             } else {
+//                 // Capture the image
+//                 const context = canvas.getContext('2d');
+//                 context.drawImage(video, 0, 0, canvas.width, canvas.height);
+//                 const imageData = canvas.toDataURL('image/jpeg');
 
-                // Stop the camera
-                if (stream) {
-                    stream.getTracks().forEach(track => track.stop());
-                }
+//                 // Stop the camera
+//                 if (stream) {
+//                     stream.getTracks().forEach(track => track.stop());
+//                 }
 
-                video.classList.add('hidden');
-                cameraBtn.textContent = "Open Camera";
-                cameraOn = false;
+//                 video.classList.add('hidden');
+//                 cameraBtn.textContent = "Open Camera";
+//                 cameraOn = false;
 
-                // Send image to Flask
-                fetch('/analyze', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ image: imageData })
-                })
-                .then(res => res.json())
-                .then(data => {
-                    resultText.textContent = data.mood
-                        ? `Detected Mood: ${data.mood}`
-                        : `Error: ${data.error}`;
-                    console.log(data.mood)
-                })
-                .catch(err => {
-                    resultText.textContent = "Error: " + err;
-                });
-            }
-        };
-    };
+//                 // Send image to Flask
+//                 fetch('/analyze', {
+//                     method: 'POST',
+//                     headers: { 'Content-Type': 'application/json' },
+//                     body: JSON.stringify({ image: imageData })
+//                 })
+//                 .then(res => res.json())
+//                 .then(data => {
+//                     resultText.textContent = data.mood
+//                         ? `Detected Mood: ${data.mood}`
+//                         : `Error: ${data.error}`;
+//                     console.log(data.mood)
+//                 })
+//                 .catch(err => {
+//                     resultText.textContent = "Error: " + err;
+//                 });
+//             }
+//         };
+//     };
--- static/js/index.js
+++ static/js/index.js
@@ -5,3 +5,10 @@
     // sidebar.classList.toggle("d-flex");
   }
 
+
+  const toggleBtn = document.getElementById('themeToggle');
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('light-theme');
+    toggleBtn.textContent = document.body.classList.contains('light-theme') ? '🌞' : '🌙';
+  });
+
